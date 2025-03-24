b5f0d552d4b4f476dab70165a41dffcbdc436663
Henry Augusta <henryaugusta4@gmail.com>
2025-03-24 15:21:05 +0700

    Add NIK to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('nip')->unique();
             $table->string('email')->unique();
+            $table->string('nik')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');  //1 for admin, // 2 for pegawai , // 3 for user
